bd33f5bc388dfc2a0a0e3a817b3678e98c46ece2
PolinaD <PolinaD@DESKTOP-0HQA69H>
2025-11-19 19:55:17 +0600

    Класс "Эксперт" является наследником от класса User. Создается отдельное поле, которое будет только у этого класса - приватное множество нраправлений эксперта (expertDirections) с типом данных мнодество строк, которое нельзя изменять. Set использовано для того, чтобы виды направлений не повторялись, так как один эксперт не может быть дважды экспертом по математике. Он автоматически исключает дубликаты. В конструкторе класса Participant вызывается конструктор родительского класса User через super - инизиализирует поля родительского класса. А также инициализируется собственного поля - expertDirections. Для его инизиализации используется HashSet, так как копирует содержимое объекта, а не ссылку на него, также по ТЗ данные не должны изменяться. Метод getExpertDirections возвращает неизменяемое множество направлений.Внешний код не сможет добавить/удалить элементы.

---
+++ academy1/src/academy/tochkavhoda/competition/model/Expert.java
@@ -0,0 +1,17 @@
+package academy.tochkavhoda.competition.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Expert extends User{
+    private Set<String> expertDirections; //Множество направлений экспертов
+
+    public Expert(String firstName, String lastName, String login, String password, Set<String> expertDirections){
+        super(firstName, lastName, login, password);
+        this.expertDirections = new HashSet<>(expertDirections); //HashSet - копирует содержимое объекта и не похволяет его изменить
+    }
+
+    public Set<String> getExpertDirections() {
+        return new HashSet<>(expertDirections);
+    }
+}
