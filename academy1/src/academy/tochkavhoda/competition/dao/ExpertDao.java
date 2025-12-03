538dccfa663258fac19935860dc2ebd369333b83
PolinaD <PolinaD@DESKTOP-0HQA69H>
2025-12-03 09:59:31 +0600

    Изменение типа методов getByLogin и getById на Expert. P.S логика интерфейсов одинаковая, поэтому при копировании забыла поменять типы у методов

--- academy1/src/academy/tochkavhoda/competition/dao/ExpertDao.java
+++ academy1/src/academy/tochkavhoda/competition/dao/ExpertDao.java
@@ -5,7 +5,7 @@ import academy.tochkavhoda.competition.model.Participant;
 
 public interface ExpertDao {
     void insert(Expert e); //добавить нового эксперта в "хранилище".
-    Participant getByLogin(String login); //найти эксперта по логину
-    Participant getById(String id); //найти эксперта по id
+    Expert getByLogin(String login); //найти эксперта по логину
+    Expert getById(String id); //найти эксперта по id
     void remove(String id); //удалить эксперта из системы
 }
