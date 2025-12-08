46d590f1ecd8b4acf723e2973d1562360f13a28b
PolinaD <PolinaD@DESKTOP-0HQA69H>
2025-12-08 23:20:20 +0600

    Класс InMemoryParticipantDao - реализация шаблона/паттерна Data Access Object для экспертов, работающая в оперативной памяти. Логика класса - версия сервера, в которой все данные сервера хранятся в оперативной памяти. Поэтому используется HashMap, а не PostgreSQL, файл и т.д. Класс наследуется от интерфейса ExpertDao и реализует все его методы. Создается приватное поле experts с типом Map<String, Expert>, где String - уникальный логин эксперта, а Expert - весь эксперт с его данными, и в качестве его значения создаём пустую карту в памяти при создании объекта DAO. Ссылку на карту/словарь Map нельзя изменить, но содержимое можно изменить. Эксперты не могут регистрироваться два или более раза на сервере. Логин уникален - подходит как ключ в HashMap. Реализация методов: 1 метод. Переопределение метода insert - добавление нового эксперта. Далее передача эксперта в карту с ключом e.getLogin() и значением Expert. Логика метода заключается в том, что, когда эксперт регистрируется, сервис вызывает expertDao.insert(expert), и эксперт добавляется в карту. 2 метод. Переопределение метода getByLogin - поиск эксперта по логину. Метод возвращает значение по ключу (переданному значению логина experts.get(login)). Если такого эксперта нет - возвращает null. Логика метода заключается в том, что при логине нужно найти эксперта и проверить пароль, при регистрации — проверить: нет ли уже такого логина. 3 метод. Переопределение метода getById - поиск эксперта по переданному id (experts.get(id)). Если такого участника нет - возвращает null. 4 метод. Переопределение метода remove - удаление эксперта из системы. Удаляется пара "ключ-значение" из карты по ключу (по логину). Логика заключается в том, что в случае если зарегистрированный на сервере эксперт покидает сервер, вся информация о нем удаляется.

---
+++ academy1/src/academy/tochkavhoda/competition/daoimpl/InMemoryExpertDao.java
@@ -0,0 +1,23 @@
+package academy.tochkavhoda.competition.daoimpl;
+
+import academy.tochkavhoda.competition.dao.ExpertDao;
+import academy.tochkavhoda.competition.model.Expert;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryExpertDao implements ExpertDao {
+    private final Map<String, Expert> experts = new HashMap<>();
+
+    @Override
+    public void insert(Expert e) {experts.put(e.getLogin(), e);}
+
+    @Override
+    public Expert getByLogin(String login){return experts.get(login);}
+
+    @Override
+    public Expert getById (String id) {return experts.get(id);}
+
+    @Override
+    public void remove(String id) { experts.remove(id);}
+
+}
