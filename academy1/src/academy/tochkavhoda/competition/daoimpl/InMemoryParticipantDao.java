aa0f120ea5623e382fe467b1b9ac77add3badb10
PolinaD <PolinaD@DESKTOP-0HQA69H>
2025-12-08 23:11:26 +0600

    Класс InMemoryParticipantDao - реализация шаблона/паттерна Data Access Object для участников, работающая в оперативной памяти. Логика класса - версия сервера, в которой все данные сервера хранятся в оперативной памяти. Поэтому используется HashMap, а не PostgreSQL, файл и т.д. Класс наследуется от интерфейса ParticipantDao и реализует все его методы. Создается приватное поле participants с типом Map<String, Participant>, где String - уникальный логин участник, а Participant - весь участник с его данными, и в качестве его значения создаём пустую карту в памяти при создании объекта DAO. Ссылку на карту/словарь Map нельзя изменить, но содержимое можно изменить. Участники не могут регистрироваться два или более раза на сервере. Логин уникален - подходит как ключ в HashMap. Реализация методов: 1 метод. Переопределение метода insert - добавление нового участника. Далее передача участника в карту с ключом p.getLogin() и значением Participant. Логика метода заключается в том, что, когда участник регистрируется, сервис вызывает participantDao.insert(participant), и участник добавляется в карту. 2 метод. Переопределение метода getByLogin - поиск участника по логину. Метод возвращает значение по ключу (переданному значению логина participants.get(login)). Если такого участника нет - возвращает null. Логика метода заключается в том, что при логине нужно найти участника и проверить пароль, при регистрации — проверить: нет ли уже такого логина. 3 метод. Переопределение метода getById - поиск участника по переданному id (participants.get(id)). Если такого участника нет - возвращает null. 4 метод. Переопределение метода remove - удаление участника из системы. Удаляется пара "ключ-значение" из карты по ключу (по логину). Логика заключается в том, что в случае если зарегистрированный на сервере участник покидает сервер, вся информация о нем удаляется, а список сделанных им заявок уничтожается.

---
+++ academy1/src/academy/tochkavhoda/competition/daoimpl/InMemoryParticipantDao.java
@@ -0,0 +1,22 @@
+package academy.tochkavhoda.competition.daoimpl;
+
+import academy.tochkavhoda.competition.model.Participant;
+import academy.tochkavhoda.competition.dao.ParticipantDao;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryParticipantDao implements ParticipantDao{
+    private final Map<String, Participant> participants = new HashMap<>();
+
+    @Override
+    public void insert(Participant p) {participants.put(p.getLogin(), p);} // Добавляем или перезаписываем
+
+    @Override
+    public Participant getByLogin(String login){return participants.get(login);}
+
+    @Override
+    public Participant getById(String id){return participants.get(id);}
+
+    @Override
+    public void remove(String id) {participants.remove(id);}
+}
