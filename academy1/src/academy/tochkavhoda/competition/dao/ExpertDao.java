1f870bea0f277a830d4e7152e8905ed52f5783d8
PolinaD <PolinaD@DESKTOP-0HQA69H>
2025-12-14 21:14:44 +0600

    Изменения: 1. В переменной id изменила тип со String на Integer, так как id - число.

--- academy1/src/academy/tochkavhoda/competition/dao/ExpertDao.java
+++ academy1/src/academy/tochkavhoda/competition/dao/ExpertDao.java
@@ -5,6 +5,6 @@ import academy.tochkavhoda.competition.model.Expert;
 public interface ExpertDao {
     void insert(Expert e); //добавить нового эксперта в "хранилище".
     Expert getByLogin(String login); //найти эксперта по логину
-    Expert getById(String id); //найти эксперта по id
-    void remove(String id); //удалить эксперта из системы
+    Expert getById(Integer id); //найти эксперта по id
+    void remove(Integer id); //удалить эксперта из системы
 }
